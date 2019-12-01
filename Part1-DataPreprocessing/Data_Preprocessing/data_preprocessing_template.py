8da95e4d4af9f4d0015867a3070d701a30fbadcd
lavish.yadav <lavish_yadav@gap.com>
2019-12-01 17:06:57 +0530

    Improved DataPreprocessing template

--- Part1-DataPreprocessing/Data_Preprocessing/data_preprocessing_template.py
+++ Part1-DataPreprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -11,26 +11,41 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
-from sklearn.preprocessing import Imputer
+from sklearn.impute import SimpleImputer
 
-imputer=Imputer(missing_values="NaN", strategy="mean", axis=0 )
-imputer = imputer.fit(X[:,1:3])
-X[:, 1:3] = imputer.transform(X[:,1:3])
+missingvalues = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0)
+missingvalues = missingvalues.fit(X[:, 1:3])
+X[:, 1:3]=missingvalues.transform(X[:, 1:3])
 
 #Taking care of categorical data
-
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.compose import ColumnTransformer, make_column_transformer
+#Converted string values to numerical data using LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.compose import ColumnTransformer
 
 labelencoder_X= LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:,0])
 
+#Used onehotencoder to split the categorical data based on numeric values
+ct=ColumnTransformer([('encoder',OneHotEncoder(),[0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X), dtype=np.float)
 
-oneHotEncoder = OneHotEncoder(categorical_features=[0])
-X = oneHotEncoder.fit_transform(X).toarray()
-
+#Used labelencoder to convert the string into categorical data
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+#y_train = sc_y.fit_transform(y_train.reshape(-1,1))
 
 # # Splitting the dataset into the Training set and Test set
 # from sklearn.cross_validation import train_test_split
