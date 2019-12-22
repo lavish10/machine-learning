d7509c4681331c580d540ba20d62499630304e11
lavish.yadav <lavish_yadav@gap.com>
2019-12-22 18:10:58 +0530

    [Lavish] Multiple Linear Regression, backward elimination, automated with adjusted R square

--- Part2-Regression/Section5-MultipleLinearRegression/multiple_linear_regression.py
+++ Part2-Regression/Section5-MultipleLinearRegression/multiple_linear_regression.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec  6 08:57:13 2019
+#!/usr/bin/env python
+# coding: utf-8
 
-@author: lavish.yadav
-"""
+# In[93]:
 
+
+# get_ipython().system('jupyter nbconvert --to script config_template.ipynb')
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -14,6 +13,10 @@ import pandas as pd
 dataset = pd.read_csv("50_Startups.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
+
+
+# In[94]:
+
 
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -28,6 +31,10 @@ X = np.array(ct.fit_transform(X), dtype=np.float)
 # Avoiding dummy variable trap
 X = X[:, 1:]
 
+
+# In[95]:
+
+
 # Splitting the dataset into training and testing
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
@@ -41,4 +48,38 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
 
+# In[96]:
 
+
+#Starting backward elimination
+import statsmodels.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int), values= X, axis =1)
+
+def backwardElimination(x, SL):
+    numVars = len(x[0])
+    temp = np.zeros((50,6)).astype(int)
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        adjR_before = regressor_OLS.rsquared_adj.astype(float)
+        if maxVar > SL:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    temp[:,j] = x[:, j]
+                    x = np.delete(x, j, 1)
+                    tmp_regressor = sm.OLS(y, x).fit()
+                    adjR_after = tmp_regressor.rsquared_adj.astype(float)
+                    if (adjR_before >= adjR_after):
+                        x_rollback = np.hstack((x, temp[:,[0,j]]))
+                        x_rollback = np.delete(x_rollback, j, 1)
+                        print (regressor_OLS.summary())
+                        return x_rollback
+                    else:
+                        continue
+    regressor_OLS.summary()
+    return x
+
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)
+print(X_Modeled)
